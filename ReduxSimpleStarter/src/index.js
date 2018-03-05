84d1f7e660ae5f191a7af01f88ab165129c8e239
Brendon Barreto Ribeiro <brendonbarreto@hotmail.com>
2018-03-05 18:53:43 -0300

    youtube search api

--- ReduxSimpleStarter/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const API_KEY = 'AIzaSyBnuS1TUkIonlZTGKePnNpvArJSR-W9SPo'
-const App = () => {
-  return <div>Hi!</div>
-}
-
-ReactDOM.render(<App />, document.querySelector('.container'))
---
+++ youtube-search/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import _ from 'lodash'
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+const API_KEY = 'Cool API Key'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+
+    this.videoSearch('rakin')
+  }
+
+  videoSearch(term) {
+    YTSearch({
+      key: API_KEY,
+      term: term
+    }, (videos) => {
+      this.setState({videos: videos, selectedVideo: videos[0]})
+    })
+  }
+
+  render() {
+    return <div>
+      <SearchBar onSearchTermChange={_.debounce((term) => this.videoSearch(term), 300) }/>
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
+    </div>
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'))
